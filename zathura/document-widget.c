dcf5f726f583261d36c838ca383cf3947932c7e9
Josh <joshua.childs01@outlook.com>
2025-12-02 19:44:28 +1100

    cleanup widget

--- zathura/document-widget.c
+++ zathura/document-widget.c
@@ -42,6 +42,7 @@ static void zathura_document_widget_size_allocate(GtkWidget* widget, GtkAllocati
 static void zathura_document_widget_container_add(GtkContainer* container, GtkWidget* widget);
 static void zathura_document_widget_container_remove(GtkContainer* container, GtkWidget* widget);
 static void zathura_document_widget_container_forall(GtkContainer* container, gboolean include_internals, GtkCallback callback, gpointer user_data);
+static void zathura_document_widget_dispose(GObject* object);
 
 enum properties_e {
   PROP_0,
@@ -60,6 +61,7 @@ static void zathura_document_widget_class_init(ZathuraDocumentClass* class) {
   GObjectClass* object_class = G_OBJECT_CLASS (class);
   object_class->set_property = zathura_document_widget_set_property;
   object_class->get_property = zathura_document_widget_get_property;
+  object_class->dispose      = zathura_document_widget_dispose;
 
   GtkContainerClass* container_class = GTK_CONTAINER_CLASS (class);
   container_class->add               = zathura_document_widget_container_add;
@@ -311,8 +313,16 @@ static void zathura_document_widget_size_allocate(GtkWidget* widget, GtkAllocati
       .height = row_line.size
     };
 
-    bool x_overlap = (page_alloc.x <= allocation->x + allocation->width && allocation->x <= page_alloc.x + page_alloc.width);
-    bool y_overlap = (page_alloc.y <= allocation->y + allocation->height && allocation->y <= page_alloc.y + page_alloc.height);
+    /*
+     * Check if 'page_alloc' and 'allocation' overlap, 
+     * which occurs iff the x-range and y-range for the 
+     * allocations overlap. For two intervals [a1, a2], 
+     * [b1, b2], they overlap iff (a1 <= b2) && (b1 <= a2)
+     */
+    bool x_overlap = (page_alloc.x <= allocation->x + allocation->width)
+      && (allocation->x <= page_alloc.x + page_alloc.width);
+    bool y_overlap = (page_alloc.y <= allocation->y + allocation->height) 
+      && (allocation->y <= page_alloc.y + page_alloc.height);
 
     gtk_widget_set_visible(page_widget, x_overlap && y_overlap);
     gtk_widget_size_allocate(page_widget, &page_alloc);
@@ -355,6 +365,16 @@ static void zathura_document_widget_container_forall(GtkContainer* container, gb
 
     callback(page_widget, user_data);
   }
+}
+
+static void zathura_document_widget_dispose(GObject* object) {
+  ZathuraDocument* document = ZATHURA_DOCUMENT(object);
+  ZathuraDocumentPrivate* priv = zathura_document_widget_get_instance_private(document);
+
+  g_free(priv->col_widths);
+  g_free(priv->row_heights);
+
+  G_OBJECT_CLASS(zathura_document_widget_parent_class)->dispose(object);
 }
 
 void zathura_document_widget_refresh_layout(ZathuraDocument* document) {
