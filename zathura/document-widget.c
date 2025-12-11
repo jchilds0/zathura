c2c64a39b5a389696b117340ed1a46f2bdb4ceef
Josh <joshua.childs01@outlook.com>
2025-12-11 18:06:26 +1100

    set page visible

--- zathura/document-widget.c
+++ zathura/document-widget.c
@@ -340,6 +340,7 @@ static void zathura_document_widget_size_allocate(GtkWidget* widget, GtkAllocati
     bool y_overlap = (page_alloc.y <= allocation->y + allocation->height) 
       && (allocation->y <= page_alloc.y + page_alloc.height);
 
+    zathura_page_set_visibility(page, x_overlap && y_overlap);
     gtk_widget_set_visible(page_widget, x_overlap && y_overlap);
     gtk_widget_size_allocate(page_widget, &page_alloc);
   }
